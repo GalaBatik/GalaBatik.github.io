9cce7b8e0bb6cd108fcf2bfee37fcc2646a8d83e
satriamjati <39191313+satriamjati@users.noreply.github.com>
2024-06-12 15:23:28 +0000

    Deploying to gh-pages from @ GalaBatik/GalaBatik.github.io@aa8c8a2bd7ab22b766cc6ad59a94ebcf409928e3 🚀

---
+++ _next/static/bCsDEC4D6sIdFdxj1vco2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-e1c5a0aea4de2ce0.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
